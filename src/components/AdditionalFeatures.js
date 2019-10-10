c4b805c00f047bf40c14687d9fb43e0975406475
spenclark <spenclark@hotmail.com>
2019-10-10 12:34:24 -0700

    mvp (almost)

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -2,13 +2,17 @@ import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
 const AdditionalFeatures = props => {
+  console.log(props)
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.store.length ? (
         <ol type="1">
           {props.store.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature
+             key={item.id} 
+             feature={item}
+             add={props.addFeature} />
           ))}
         </ol>
       ) : (
